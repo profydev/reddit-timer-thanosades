8548a7e0ef2bd01e407c2892c0d18151484fa72d
Thanos Dimitriades <thanosades@gmail.com>
2021-09-01 15:01:44 +0300

    add styled-components, styled-normalize and GlobalStyle

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import theme from './theme';
 import Home from './components/Home';
 import Search from './components/Search';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/search">
-            <Search />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route>404 - Not Found</Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
