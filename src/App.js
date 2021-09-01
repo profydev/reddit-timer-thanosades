cc522a7e95eda400d58ecd64793e57fe44e56ac1
Thanos Dimitriades <89385573+thanosades@users.noreply.github.com>
2021-09-01 17:09:25 +0300

    Create app skeleton (#1)
    * install Bitter and Montserrat fonts
    
    * add normalize to index.css
    
    * add router
    
    * fix Route bug
    
    * replace ttf fonts with woff and woff2
    
    * add styled-components, styled-normalize and GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import theme from './theme';
+import Home from './components/Home';
+import Search from './components/Search';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route>404 - Not Found</Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
