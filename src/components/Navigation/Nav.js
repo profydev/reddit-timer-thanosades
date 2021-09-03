93fd4bff6739e9447bf162774177131317f0717d
Thanos Dimitriades <thanosades@gmail.com>
2021-09-03 22:16:10 +0300

    implements navigation skeleton

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Nav() {
   return (
     <nav>
-      <ul>
-        <li>Option 1</li>
-        <li>Option 2</li>
-        <li>Option 3</li>
-      </ul>
+      <List>
+        <li>Search</li>
+        <li>How it works</li>
+        <li>About</li>
+      </List>
     </nav>
   );
 }
+
+const List = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+
+  &>li:not(:last-child) {
+    margin-right: 25px;
+  }
+`;
