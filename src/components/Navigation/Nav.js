fa983007aa2c0cbba8e95da155852d76891437b6
Thanos Dimitriades <thanosades@gmail.com>
2021-09-13 18:35:54 +0300

    add HashLink and optimize svg

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import HorizontalList from './HorizontalList';
 
 export default function Nav() {
   return (
     <nav>
       <HorizontalList>
-        <li><Link to="/search/javascript">Search</Link></li>
-        <li><a href="#how-it-works">How it works</a></li>
-        <li><a href="#about">About</a></li>
+        <li><Link to="/search">Search</Link></li>
+        <li><HashLink to="#how-it-works">How it works</HashLink></li>
+        <li><HashLink href="#about">About</HashLink></li>
       </HorizontalList>
     </nav>
   );
